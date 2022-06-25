8b8774b446de435c4e7acf656c09ba41501fd174
Omar Zeb <omar_zeb@hotmail.com>
2022-06-25 17:08:33 +0500

    performed autopep8

--- churn_library.py
+++ churn_library.py
@@ -22,12 +22,16 @@ from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import plot_roc_curve, classification_report
 
-os.environ['QT_QPA_PLATFORM']='offscreen'
+os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
-logging.basicConfig(filename=os.path.join(os. getcwd(), "logs", "churn_library.log"),
-                        filemode="w",
-                        level = logging.INFO,
-                        format='%(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    filename=os.path.join(
+        os. getcwd(),
+        "logs",
+        "churn_library.log"),
+    filemode="w",
+    level=logging.INFO,
+    format='%(name)s - %(levelname)s - %(message)s')
 
 
 def import_data(pth):
@@ -38,7 +42,7 @@ def import_data(pth):
             pth: a path to the csv
     output:
             df: pandas dataframe
-    '''	
+    '''
     try:
         logging.Info("Reading csv {}".format(pth))
         assert isinstance(pth, str)
@@ -68,41 +72,44 @@ def perform_eda(df):
         assert isinstance(df, pd.DataFrame)
         logging.Info("Input dataframe is correct")
 
-        #print first five rows of the dataframe
-        print("First five rows of data are: ",df.head())
+        # print first five rows of the dataframe
+        print("First five rows of data are: ", df.head())
 
         print("The shape of the data is:", df.shape())
 
-        print("Total number of null entries per column in the data are:", df.isnull().sum())
+        print(
+            "Total number of null entries per column in the data are:",
+            df.isnull().sum())
 
         print("Data Summary is:", df.describe())
 
-        df['Churn'] = df['Attrition_Flag'].apply(lambda val: 0 if val == "Existing Customer" else 1)
+        df['Churn'] = df['Attrition_Flag'].apply(
+            lambda val: 0 if val == "Existing Customer" else 1)
 
         image_dir = os.path.join(os.cwd, "images")
         # Plot the histogram of Customer Churn
-        plt.figure(figsize=(20,10)) 
+        plt.figure(figsize=(20, 10))
         df['Churn'].hist()
         plt.title("Distribution of Churn")
         plt.savefig(os.path.join(image_dir, "Churn_Hist.png"))
         plt.close()
 
         # Plot the histogram of Customer Age
-        plt.figure(figsize=(20,10)) 
+        plt.figure(figsize=(20, 10))
         df['Customer_Age'].hist()
         plt.title("Age Distribution of Customers")
         plt.savefig(os.path.join(image_dir, "Age_Hist.png"))
         plt.close()
 
         # Plot the histogram of Customer Marital Status
-        plt.figure(figsize=(20,10)) 
+        plt.figure(figsize=(20, 10))
         df.Marital_Status.value_counts('normalize').plot(kind='bar')
         plt.title("Matital Distribution of Customers")
         plt.savefig(os.path.join(image_dir, "Marital_Hist.png"))
         plt.close()
 
         # Plot The transaction distribtion
-        plt.figure(figsize=(20,10)) 
+        plt.figure(figsize=(20, 10))
         sns.histplot(df['Total_Trans_Ct'], stat='density', kde=True)
         plt.title(" Total Trans Ct distribution")
         plt.savefig(os.path.join(image_dir, "Trans_CT.png"))
@@ -115,8 +122,7 @@ def perform_eda(df):
         plt.savefig(os.path.join(image_dir, "HeatMap.png"))
         plt.close()
 
-
-    except:
+    except BaseException:
         logging.ERROR("ERROR: Dataframe not found")
 
 
@@ -133,15 +139,15 @@ def encoder_helper(df, category_lst, response):
     output:
             df: pandas dataframe with new columns for
     '''
-    
+
     for category in category_lst:
         out_list = []
         groups = df.groupby(category).mean()[response]
 
         for val in df['Gender']:
             out_list.append(groups.loc[val])
-        
-        df["{}_{}".format(category,response)] = out_list
+
+        df["{}_{}".format(category, response)] = out_list
 
     return df
 
@@ -192,20 +198,34 @@ def perform_feature_engineering(df, response):
 
     up_df = encoder_helper(df, cat_columns, response)
 
-
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-             'Total_Relationship_Count', 'Months_Inactive_12_mon',
-             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
-             'Income_Category_Churn', 'Card_Category_Churn']
+    keep_cols = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
 
     X[keep_cols] = up_df[keep_cols]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
 
     return X_train, X_test, y_train, y_test
+
 
 def classification_report_image(y_train,
                                 y_test,
@@ -236,16 +256,16 @@ def classification_report_image(y_train,
 
     plt.text(0.01, 1.25, str('Logistic Regression Training Results'))
 
-    plt.text(0.01, 0.05, str(classification_report(y_test, 
-                                                y_test_preds_lr)))
+    plt.text(0.01, 0.05, str(classification_report(y_test,
+                                                   y_test_preds_lr)))
 
     plt.text(0.01, 0.6, str('Logistic Regression Testing Results'))
-    plt.text(0.01, 0.7, str(classification_report(y_train, 
-                                                y_train_preds_lr)))
+    plt.text(0.01, 0.7, str(classification_report(y_train,
+                                                  y_train_preds_lr)))
 
     plt.axis('off')
-    plt.savefig(os.path.join(image_dir,'logistic_regression_results.png'))
-    plt.close()   
+    plt.savefig(os.path.join(image_dir, 'logistic_regression_results.png'))
+    plt.close()
 
     # Plot the results of random forest classifier
     plt.figure()
@@ -253,15 +273,15 @@ def classification_report_image(y_train,
 
     plt.text(0.01, 1.25, str('Random Forest Training Results'))
 
-    plt.text(0.01, 0.05, str(classification_report(y_test, 
-                                                y_test_preds_rf)))
+    plt.text(0.01, 0.05, str(classification_report(y_test,
+                                                   y_test_preds_rf)))
 
     plt.text(0.01, 0.6, str('Random Forest Testing Results'))
-    plt.text(0.01, 0.7, str(classification_report(y_train, 
-                                                y_train_preds_rf)))
+    plt.text(0.01, 0.7, str(classification_report(y_train,
+                                                  y_train_preds_rf)))
 
     plt.axis('off')
-    plt.savefig(os.path.join(image_dir,'random_forest_results.png'))
+    plt.savefig(os.path.join(image_dir, 'random_forest_results.png'))
     plt.close()
 
 
@@ -284,7 +304,7 @@ def feature_importance_plot(model, X_data, output_pth):
     names = [X_data.columns[i] for i in indices]
 
     # Create plot
-    plt.figure(figsize=(20,5))
+    plt.figure(figsize=(20, 5))
 
     # Create plot title
     plt.title("Feature Importance")
@@ -299,6 +319,7 @@ def feature_importance_plot(model, X_data, output_pth):
     # Save the figure
     plt.savefig(output_pth)
     plt.close()
+
 
 def train_models(X_train, X_test, y_train, y_test):
     '''
@@ -315,12 +336,12 @@ def train_models(X_train, X_test, y_train, y_test):
     rfc = RandomForestClassifier(random_state=42)
     lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
 
-    param_grid = { 
-    'n_estimators': [200, 500],
-    'max_features': ['auto', 'sqrt'],
-    'max_depth' : [4,5,100],
-    'criterion' :['gini', 'entropy']
-     }
+    param_grid = {
+        'n_estimators': [200, 500],
+        'max_features': ['auto', 'sqrt'],
+        'max_depth': [4, 5, 100],
+        'criterion': ['gini', 'entropy']
+    }
 
     # Random Forest
     cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
@@ -337,15 +358,15 @@ def train_models(X_train, X_test, y_train, y_test):
 
     # save the roc curve with score
     image_dir = os.path.join(os.cwd(), "images")
-    
+
     lrc_plot = plot_roc_curve(lrc, X_test, y_test)
-    
+
     plt.figure(figsize=(15, 8))
     axis = plt.gca()
-    
+
     _ = plot_roc_curve(cv_rfc.best_estimator_,
                        X_test, y_test, ax=axis, alpha=0.8)
-    
+
     lrc_plot.plot(ax=axis, alpha=0.8)
     plt.savefig(os.path.join(image_dir, 'roc_curve.png'))
     plt.close()
@@ -353,8 +374,12 @@ def train_models(X_train, X_test, y_train, y_test):
     logging.info("Save best model.")
 
     model_dir = os.path.join(os.cwd(), "model")
-    joblib.dump(cv_rfc.best_estimator_, os.path.join(model_dir,'rfc_model.pkl'))
-    joblib.dump(lrc, os.path.join(model_dir,'logistic_model.pkl'))
+    joblib.dump(
+        cv_rfc.best_estimator_,
+        os.path.join(
+            model_dir,
+            'rfc_model.pkl'))
+    joblib.dump(lrc, os.path.join(model_dir, 'logistic_model.pkl'))
 
     # store model results
     classification_report_image(
@@ -367,7 +392,7 @@ def train_models(X_train, X_test, y_train, y_test):
 
     # store feature importances plot
     feature_importance_plot(cv_rfc.best_estimator_, X_train,
-                            os.path.join(image_dir,'feature_importances.png'))
+                            os.path.join(image_dir, 'feature_importances.png'))
 
 
 if __name__ == "__main__":
